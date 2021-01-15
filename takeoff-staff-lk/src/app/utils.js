bd71fe26129af584ccf67d7b6f6bbde5503ba404
michael-916310 <916310@rambler.ru>
2021-01-15 12:33:00 +0400

    feat: контакты выбираются из json на сервере

--- takeoff-staff-lk/src/app/utils.js
+++ takeoff-staff-lk/src/app/utils.js
@@ -15,31 +15,17 @@ export async function login (userName, userPsw) {
 }
 
 export async function getContactData(){
-  let list = [
-    {
-      contactId: 1,
-      contactName: 'Иванов Иван Иванович',
-      contactPhone: '+7 917 999-88-77'
-    },
-    {
-      contactId: 2,
-      contactName: 'Чингачгук Петр Петрович',
-      contactPhone: '+7 927 111-22-33'
-    },
-    {
-      contactId: 3,
-      contactName: 'Васильева Василиса Премудрая',
-      contactPhone: '+7 927 222-22-33'
-    },
-    {
-      contactId: 4,
-      contactName: 'Царь Царевич Король Королевич',
-      contactPhone: '+7 927 333-22-33'
+  try{
+    const resp = await fetch("../data/contact-list-data.json");
+    if (resp.ok) {
+      const data = await resp.json();
+      console.log(data);
+      return data.list;
     }
-  ];
+    return [];
 
-  const resp = await fetch("../data/contact-list-data.json");
-  console.log(resp);
-
-  return list;
+  }catch(e){
+    console.log('Error while loading contact-list-data.json:', e);
+    return [];
+  }
 }
